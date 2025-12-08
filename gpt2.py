14b9de2e9a66df16842d6f4625ec8ef5cfa9c460
jasontribby <tribbyjason@gmail.com>
2025-12-08 17:22:59 +0100

    Refactor gpt2.py by removing unused components
    Removed dropout parameters and CausalSelfAttention class.

--- gpt2.py
+++ gpt2.py
@@ -65,28 +65,11 @@ class GPT2(nn.Module):
                  max_content_len=1024,
                  vocab_size=50257):
                  heads = 12
-                 dropout_att float = 0.1 # dropout helps 
-                 dropout_res float = 0.1
-                 dropout_emb float = 0.1
+
         super().__init__()
 
         self.wte = nn.Embedding(vocab_size, d_model)
         self.wpe = nn.Embedding(max_content_len, d_model)
                      
-class CausalSelfAttention(nn.Module):
-    def __init__(self, config: GPT2):
-        super()._init_()
-        assert config.d_model % config.heads == 0
-        self.heads = config.heads
-        self.heads_dim = config.d_model // config.heads
-        self.scale = 1.0/ (self.heads_dim ** 0.5)
-        self.attc = nn.Linear(config.d_model, 3 * config.d_model, bias=True)
-        self.projc = nn.Linear(config.d_model, config.d_model, bias=True)
-        self.attention_dropout = nn.Dropout(config.dropout_att)
-        self.resid_dropout = nn.Dropout(config.dropout_res)
-        self.register_buffer("mask", torch.tril(torch.ones(config.max_content_len, config.max_content_len)).view(1, 1, config.max_content_len, config.max_content_len))
-    def forward(self, x, attentionmask=None):
-        B, T, C = x.size()
-        qkv = self.attc(x)
-        q, k, v = qkv.split(C, dim=2) 
+
         
